4acf5d6fcd60334f4f54938602aef4a02e9945c4
Jon Roberts <kewlguy@gmail.com>
2020-08-20 14:10:14 -0600

    Added the first file

---
+++ lunchlady.rb
@@ -0,0 +1,13 @@
+# {name: 'string' price:float, type: "side|main"}
+
+#Disk Class, - handles both main and side disk as subclass
+
+#SideDish Class and Main Dish Class
+
+#app
+
+#Resturant?
+
+#Jot Thoughts below, ignore this:
+#@main_dishes = [ Dish.new('name of food goes here', <price goes here>), Dish.new('name of food goes here', <price goes here>) ]
+#@side_dishes = [ Dish.new('name of side dish goes here', <price goes here>), Dish.new('name of food goes here', <price goes here>)]
