310b781a48ca156c0b3061bf4f5d8d10f7fcee0f
Jon Roberts <kewlguy@gmail.com>
2020-08-20 15:04:33 -0600

    cleaning up the class, adding my TODO comments

--- lunchlady.rb
+++ lunchlady.rb
@@ -1,13 +1,32 @@
 # {name: 'string' price:float, type: "side|main"}
 
-#Disk Class, - handles both main and side disk as subclass
+#Dish Class, - handles both main and side disk as subclass
+class Dish
+    attr_accessor :name, :price
 
-#SideDish Class and Main Dish Class
+    def initialize(name, price)
+        @name = name
+        @price = price
+      #  @desc
+      #  @nutinfo
 
-#app
+    end
+end
 
-#Resturant?
+#Setting up "Menu"
+@main_dishes = [Dish.new('Cheeseburger', 5.99), Dish.new("Chicken Sandwich", 5.99)]
+@side_dishes = [Dish.new('French Fries', 1.99), Dish.new("Mac and Cheese", 2.49)]
+p @main_dishes
+p @side_dishes
 
-#Jot Thoughts below, ignore this:
-#@main_dishes = [ Dish.new('name of food goes here', <price goes here>), Dish.new('name of food goes here', <price goes here>) ]
-#@side_dishes = [ Dish.new('name of side dish goes here', <price goes here>), Dish.new('name of food goes here', <price goes here>)]
+
+# print @main_dishes, ask for user selection, push the selection into basket array
+
+# print @side_dishes, ask for user selection push the selection into basket array, 
+# repeat one more time.
+
+
+# after selecting one main and two side dishes, print the "basket" array
+
+# total the pricing on the order that is in the basket
+
