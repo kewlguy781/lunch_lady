44211c06b946a42d1dc48859a0fdf2dd0907d417
Jon Roberts <kewlguy@gmail.com>
2020-08-20 17:45:28 -0600

    added total price, cleaning up stuff

--- lunchlady.rb
+++ lunchlady.rb
@@ -14,8 +14,8 @@ class Dish
 end
 
 #Setting up "Menu"
-@main_dishes = [Dish.new('Cheeseburger', 5.99), Dish.new("Chicken Sandwich", 5.99)]
-@side_dishes = [Dish.new('French Fries', 1.99), Dish.new("Mac and Cheese", 2.49)]
+@main_dishes = [Dish.new('Cheeseburger', 5.99), Dish.new("Chicken Sandwich", 5.99), Dish.new("Fried Chicken", 6.99), Dish.new("Fish Sticks", 4.99)]
+@side_dishes = [Dish.new('French Fries', 1.99), Dish.new("Mac and Cheese", 2.49), Dish.new("Onion Rings", 2.99)  ]
 @basket = []
 # p @main_dishes
 # p @side_dishes
@@ -47,14 +47,14 @@ end
 
 def select_dish(x)
     addbasket={}
-    puts "please select dish"
+    puts "Please Select a Dish"
     #push the select dish into "basket"
     dish_index = gets.chomp.to_i
     addbasket[:name] = "#{x[dish_index-1].name}"
     addbasket[:price] = "#{x[dish_index-1].price}"
-    p addbasket
+    #p addbasket
     @basket << addbasket
-    p @basket
+    #p @basket
 end
 
 # print @side_dishes, ask for user selection push the selection into basket array, 
@@ -66,12 +66,20 @@ end
 
 # after selecting one main and two side dishes, print the "basket" array
 def in_basket
-    puts "basket"
+    puts "You are purchasing: "
+    @basket.each do |basket|
+        puts "#{basket[:name]}      Price: #{basket[:price]}"
+    end
 end
 
 # total the pricing on the order that is in the basket
 def calc_price
-    puts "total price"
+    #puts "total price"
+    totalprice = 0.0
+    @basket.each do |basket|
+        totalprice += basket[:price].to_f
+    end
+    puts "The total price is:     $#{totalprice}"
 end
 
 start_order
