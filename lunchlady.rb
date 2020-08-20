5489d14f4aedc191c9c3c38ccc6d86a95f4df3aa
Jon Roberts <kewlguy@gmail.com>
2020-08-20 15:45:38 -0600

    the display part implemented

--- lunchlady.rb
+++ lunchlady.rb
@@ -16,10 +16,23 @@ end
 #Setting up "Menu"
 @main_dishes = [Dish.new('Cheeseburger', 5.99), Dish.new("Chicken Sandwich", 5.99)]
 @side_dishes = [Dish.new('French Fries', 1.99), Dish.new("Mac and Cheese", 2.49)]
-p @main_dishes
-p @side_dishes
+# p @main_dishes
+# p @side_dishes
+# p @main_dishes.class
 
+# Jon's Note: !!!! If its Class, use <variable>.<info>, if its hash use <variable>[:<type>] !!!!
 
+def print_menu(x) 
+
+    x.each_with_index do |dish, index|
+    puts "#{index + 1}) #{dish.name} Price: #{dish.price}"
+    end
+
+end
+
+print_menu(@main_dishes)
+puts
+print_menu(@side_dishes)
 # print @main_dishes, ask for user selection, push the selection into basket array
 
 # print @side_dishes, ask for user selection push the selection into basket array, 
